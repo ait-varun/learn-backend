385065acf526e2f86468ee86de72e22d6d19cb00
varun <aitvarunbhatia@gmail.com>
2024-06-05 13:00:24 +0530

    basic structure

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about");
+});
+
+app.post("/post", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
 });
 
 app.listen(port, () => {
