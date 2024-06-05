117cc315f363f3304149a65e8a0f66df4c76b2a4
varun <aitvarunbhatia@gmail.com>
2024-06-05 16:53:09 +0530

    added users and user id page

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
+// Load environment variables
 process.loadEnvFile();
+
+// Import required modules
 import express from "express";
 import { readFileSync } from "fs";
 
+// Create an Express application
 const app = express();
 const port = process.env.PORT || 3000;
 
-// parse json data
+// Read and parse JSON data from a file
 const users = JSON.parse(readFileSync("./users.json"));
 
+// Define route to handle root endpoint
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// make api calls sending json data
-
-app.get("/users", (req, res) => {
+// Define route to get all users
+app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
+// Define route to get a specific user by ID
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  res.json(user);
+});
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
