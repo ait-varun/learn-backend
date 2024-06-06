4a86df5c06c88378afb8d624336d390615a4d68f
varun <aitvarunbhatia@gmail.com>
2024-06-06 10:19:29 +0530

    updated code

--- validation.js
+++ validation.js
@@ -4,6 +4,7 @@ const userSchema = Joi.object({
   first_name: Joi.string().required(),
   last_name: Joi.string().required(),
   email: Joi.string().email().required(),
+  gender: Joi.string().valid("Male", "Female", "Non-binary").required(),
   // Add more validation rules as needed
 });
 
