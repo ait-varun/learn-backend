7ca8a29bfcd75e3a565de85c390c9e6441ccdaa9
varun <aitvarunbhatia@gmail.com>
2024-06-07 12:57:10 +0530

    updated the code

--- index.js
+++ index.js
@@ -18,40 +18,13 @@
 // Load environment variables from a file
 process.loadEnvFile();
 
-
-
 import express from "express";
-import fs from "fs/promises";
 import usersRouter from "./routes/users.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-app.use(async (req, res, next) => {
-  try {
-    await fs.appendFile(
-      "log.txt",
-      `\nDate :${Date.now()}, IP: ${req.ip}, Method: ${req.method}, URL: ${
-        req.url
-      }`
-    );
-    next();
-  } catch (err) {
-    console.error("Error writing to log file:", err);
-    next(err);
-  }
-});
 
 // Mount the users router on /api/users
 app.use("/", usersRouter);
