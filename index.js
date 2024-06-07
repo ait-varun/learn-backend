165215f948ad541c752bb2970cd9babaa743db81
varun <aitvarunbhatia@gmail.com>
2024-06-07 12:53:48 +0530

    updated the code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 process.loadEnvFile();
 
 import express from "express";
-import fs from "fs/promises";
 import usersRouter from "./routes/users.js";
 
 const app = express();
@@ -10,29 +9,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-app.use(async (req, res, next) => {
-  try {
-    await fs.appendFile(
-      "log.txt",
-      `\nDate :${Date.now()}, IP: ${req.ip}, Method: ${req.method}, URL: ${
-        req.url
-      }`
-    );
-    next();
-  } catch (err) {
-    console.error("Error writing to log file:", err);
-    next(err);
-  }
-});
 
 // Mount the users router on /api/users
 app.use("/", usersRouter);
